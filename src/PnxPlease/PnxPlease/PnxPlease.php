e656ca4072337abd5b53df57ecb141521bede092
James Little <j.little@miami.edu>
2016-08-29 15:55:17 -0400

    Adding methods for getting array and JSON

--- src/PnxPlease/PnxPlease/PnxPlease.php
+++ src/PnxPlease/PnxPlease/PnxPlease.php
@@ -2,27 +2,47 @@
 namespace PnxPlease\PnxPlease;
 class PnxPlease {
 
-  private $recordId;
-  private $primoUrl;
-  
-  public function __construct($recordId, $primoUrl) {
-    $this->recordId = $recordId;
-    $this->primoUrl = $primoUrl;
-  }
-  
-  public function getPnx() {
-    $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, $this->createRequest());
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    $output = curl_exec($ch); 
-    curl_close($ch);
-    return $output;
-  }
-  private function createRequest() {
-    $recordId = $this->recordId;
-    $url = $this->primoUrl . "?ct=display&fn=search&doc=$recordId&showPnx=true";
-    return $url;
-  }
+    private $recordId;
+    private $primoUrl;
+    private $pnxRecord;
+    
+    
+    public function __construct($recordId, $primoUrl) {
+	$this->recordId = $recordId;
+	$this->primoUrl = $primoUrl;
+	$this->pnxRecord = $this->getPnx();
+    }
+    
+    private function getPnx() {
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $this->createRequest());
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch); 
+	curl_close($ch);
+	return $output;
+    }
 
- 
+    private function createRequest() {
+	$recordId = $this->recordId;
+	$url = $this->primoUrl . "?ct=display&fn=search&doc=$recordId&showPnx=true";
+	return $url;
+    }
+
+
+    public function toString() {
+	return $this->pnxRecord; 
+    }
+
+    public function toArray() {
+	$xml = simplexml_load_string($this->pnxRecord);
+	$pnxArray = unserialize(serialize(json_decode(json_encode((array) $xml), 1)));
+	return $pnxArray;
+    }
+
+    public function toJSON() {
+	$xml = simplexml_load_string($this->pnxRecord);
+	$pnxJSON = json_encode($xml);
+	return $pnxJSON;
+    }
+    
 }
--- tests/PnxPleaseTest.php
+++ tests/PnxPleaseTest.php
@@ -4,16 +4,42 @@ use PnxPlease\PnxPlease\PnxPlease;
 
 class PnxPleaseTest extends TestCase
 {
-  public function testThatYouGetTheRightXMLRecord()
-  {
-    // Initialize the object with a record id and a Primo URL
-    $pnxPlease = new PnxPlease("01UOML_ALMA21149602560002976","http://miami-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/display.do");
-    // Get the PNX Record
-    $pnx = trim($pnxPlease->getPnx());
-    // Load the XML file used for testing and trim whitepsace 
-    $xml = trim(file_get_contents(__DIR__ . '/record.xml'));
-    // Assert that the XML file and the PNX record from Primo are the same 
-    $this->assertEquals($xml,$pnx);
-  }
+    public function testThatYouGetTheRightXMLRecord()
+    {
+	// Initialize the object with a record id and a Primo URL
+	$pnxPlease = new PnxPlease("01UOML_ALMA21149602560002976","http://miami-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/display.do");
+	// Get the PNX Record
+	$pnx = trim($pnxPlease->toString());
+	// Load the XML file used for testing and trim whitepsace 
+	$xml = trim(file_get_contents(__DIR__ . '/record.xml'));
+	// Assert that the XML file and the PNX record from Primo are the same 
+	$this->assertEquals($xml,$pnx);
+    }
 
+    public function testThatYouCanGetAnArray()
+    {
+	// Initialize the object with a record id and a Primo URL
+	$pnxPlease = new PnxPlease("01UOML_ALMA21149602560002976","http://miami-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/display.do");
+
+	$pnxArray = $pnxPlease->toArray();
+
+	$this->assertEquals($pnxArray['control']['sourcerecordid'], '21149602560002976');
+	
+    }
+
+    public function testThatYouCanGetJSON()
+    {
+	// Initialize the object with a record id and a Primo URL
+	$pnxPlease = new PnxPlease("01UOML_ALMA21149602560002976","http://miami-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/display.do");
+
+	$pnxJSON = $pnxPlease->toJSON();
+	$testingJSON = file_get_contents(__DIR__ . '/record.json');
+
+	$this->assertEquals(trim($pnxJSON), trim($testingJSON));
+	
+    }
+
+    
+
+    
 }
